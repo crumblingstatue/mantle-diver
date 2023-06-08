affa95899f27eedd23aad4a5a488069e35a6c8bf
crumblingstatue <radiantstatue@gmail.com>
2023-06-09 00:00:51 +0200

    Increase texture atlas size to 1024x1024

--- src/texture_atlas.rs
+++ src/texture_atlas.rs
@@ -20,8 +20,8 @@ pub struct AtlasBundle {
 impl AtlasBundle {
     pub fn new(res_path: &str) -> anyhow::Result<Self> {
         let cfg = TexturePackerConfig {
-            max_width: 512,
-            max_height: 512,
+            max_width: 1024,
+            max_height: 1024,
             allow_rotation: false,
             border_padding: 0,
             texture_padding: 0,
