1b439f4abab30ae6d34cfec7abd880c9cfbb2afb
crumblingstatue <radiantstatue@gmail.com>
2023-08-08 23:02:32 +0200

    Make graphic picker close on select + close button

--- src/debug/graphic_picker.rs
+++ src/debug/graphic_picker.rs
@@ -16,28 +16,32 @@ impl GraphicPicker {
         atlas_size: crate::graphics::ScreenRes,
         res: &Res,
     ) {
-        if !self.open {
-            return;
-        }
-        egui::Window::new("Graphic picker").show(ctx, |ui| {
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                ui.horizontal_wrapped(|ui| {
-                    let mut w_accum = 0;
-                    for (name, rect) in &res.atlas.rects {
-                        ui.group(|ui| {
-                            ui.label(name);
-                            if ui.graphic_image_button(rect, atlas_size).clicked() {
-                                self.picked = Some((name.clone(), *rect));
+        let mut open = self.open;
+        egui::Window::new("Graphic picker")
+            .open(&mut open)
+            .show(ctx, |ui| {
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    ui.horizontal_wrapped(|ui| {
+                        let mut w_accum = 0;
+                        for (name, rect) in &res.atlas.rects {
+                            ui.group(|ui| {
+                                ui.label(name);
+                                if ui.graphic_image_button(rect, atlas_size).clicked() {
+                                    self.picked = Some((name.clone(), *rect));
+                                    self.open = false;
+                                }
+                            });
+                            w_accum += rect.w;
+                            if w_accum > 200 {
+                                ui.end_row();
+                                w_accum = 0;
                             }
-                        });
-                        w_accum += rect.w;
-                        if w_accum > 200 {
-                            ui.end_row();
-                            w_accum = 0;
                         }
-                    }
+                    });
                 });
             });
-        });
+        if !open {
+            self.open = false;
+        }
     }
 }
